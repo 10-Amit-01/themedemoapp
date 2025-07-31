2c5c82e636b45cd7a7b4264076c914d2edf47dc6
Amit Kumar <sahaiamit172@gmail.com>
2025-07-31 16:05:50 +0530

    route fix

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,26 +4,29 @@ import Home, { loader } from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-        loader: loader
-      },
-      {
-        path: "/about",
-        element: <About />,
-      },
-      {
-        path: "/contact",
-        element: <Contact />,
-      },
-    ],
-  },
-]);
+const routes = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      hydrateFallbackElement:<div className="m-auto">Loading...</div>,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+          loader: loader,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ],
+);
 
 export default routes;
